5c54aae940ce7edb3bb08c3fb48602cc00199ee0
Jasper Post <jasper.post@cloudnation.nl>
2026-04-29 13:57:05 +0200

    feat: add role_object_id resource as output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "role_assignments" {
   value = azurerm_role_assignment.role
 }
 
+output "role_assignment_object_ids" {
+  value = azurerm_role_assignment.role_object_id
+}
+
 output "role_definitions" {
   value = azurerm_role_definition.custom
 }
