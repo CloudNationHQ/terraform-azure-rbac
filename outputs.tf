342cd84a5bbb8229f0f27d3081764d618dcd6300
Eddy Vera <eddy.vera@cloudnation.nl>
2026-06-08 17:17:16 +0200

    feat: rm unused output

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "role_assignments" {
   value = azurerm_role_assignment.this
 }
 
-output "role_assignment_object_ids" {
-  value = azurerm_role_assignment.role_object_id
-}
-
 output "role_definitions" {
   value = azurerm_role_definition.custom
 }
